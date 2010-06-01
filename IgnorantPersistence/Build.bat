24022b66522c8500b31d6a8e57b0ff7b54ecf062
Stephen McKamey <mckamey@users.noreply.github.com>
2010-06-01 19:25:36 +0000

    - rolling back to VS2008 for compatibility

--- IgnorantPersistence/Build.bat
+++ IgnorantPersistence/Build.bat
@@ -1,11 +1,11 @@
 @ECHO off
 PUSHD "%~dp0"
 
-SET MSBuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+SET MSBuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe
 IF NOT EXIST "%MSBuild%" (
-	ECHO Installation of .NET Framework 4.0 is required to build this project, including for .NET v2.0 and v3.5
-	ECHO http://www.microsoft.com/downloads/details.aspx?FamilyID=0a391abd-25c1-4fc0-919f-b21f31ab88b7
-	START /d "~\iexplore.exe" http://www.microsoft.com/downloads/details.aspx?FamilyID=0a391abd-25c1-4fc0-919f-b21f31ab88b7
+	ECHO Installation of .NET Framework 3.5 is required to build this project
+	ECHO http://msdn.microsoft.com/en-us/netframework/cc378097.aspx
+	START /d "~\iexplore.exe" http://msdn.microsoft.com/en-us/netframework/cc378097.aspx
 	EXIT /b 1
 	GOTO END
 )
